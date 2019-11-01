92633c9e9009e5690a9bd2f0dda4c87f6ca00b63
evfjunior <evfjunior@protonmail.com>
2019-11-01 17:56:12 -0300

    Applied applications properties; Enabled local database for testing

--- src/main/java/com/evfjunior/contactmanager/model/Contact.java
+++ src/main/java/com/evfjunior/contactmanager/model/Contact.java
@@ -4,9 +4,9 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.springframework.data.annotation.Id;
 
 import javax.persistence.Entity;
+import javax.persistence.Id;
 
 @AllArgsConstructor
 @Entity
